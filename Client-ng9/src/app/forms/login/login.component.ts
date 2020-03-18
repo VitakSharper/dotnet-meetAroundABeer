06379b9007e8c318cae717b44368060c64d728c8
VitakSharper <v.banard@gmail.com>
2020-03-18 20:44:38 +0100

    added login form

--- Client-ng9/src/app/forms/login/login.component.ts
+++ Client-ng9/src/app/forms/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder) {
   }
 
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  private createForm() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onLogIn() {
+
+  }
+
+  cancelHandler() {
+
+  }
 }
