6495ca392b11c9eeac42d647941a9ae34b931f7f
VitakSharper <v.banard@gmail.com>
2020-05-02 21:33:18 +0200

    DOTNET: add pagination to backend , some fixes to client side

--- Data/DataExt.cs
+++ Data/DataExt.cs
@@ -7,24 +7,7 @@ using System.Text;
 namespace Data
 {
     internal static class DataExt
-    {
-        public static AppUser CreatePasswordHash(this AppUser user, string password)
-        {
-            using var hmac = new HMACSHA512();
-            //user.PasswordSalt = hmac.Key;
-            //user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
-            return user;
-        }
-
-        public static bool VerifyPasswordHash(this AppUser user, string password)
-        {
-            using var hmac = new HMACSHA512(new byte[] { 1, 2, 3, 5 });
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
-
-            return !computedHash.Where((t, i) => t != user.PasswordHash[i]).Any();
-        }
-
-        public static int CalculateAge(this DateTime theDateTime) =>
+    { public static int CalculateAge(this DateTime theDateTime) =>
             theDateTime.AddYears(DateTime.Today.Year - theDateTime.Year) > DateTime.Today
                 ? (DateTime.Today.Year - theDateTime.Year) - 1
                 : DateTime.Today.Year - theDateTime.Year;
